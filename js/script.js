977d143f28115246e5367196fdd9774c9ea244bd
Giovanni-dotcome <giodevito99@gmail.com>
2021-04-29 07:42:40 +0200

    joke gen

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 var app = new Vue({
     el: '#root',
     data: {
-        test: 'ocnwicnwiu'
+        color: '',
+        setup: '',
+        punchline: '',
+        visible: true,
     },
-    methods: {}
+    methods: {
+        changeColor() {
+            this.genQuote()
+            this.color = randomColor()
+        },
+        genQuote() {
+            axios.get('https://official-joke-api.appspot.com/jokes/programming/random').then(response => {
+                this.setup = response.data[0].setup
+                this.punchline = response.data[0].punchline;
+            })
+        },
+        switchJoke() {
+            this.visible = !this.visible;
+        }
+    },
+    created() {
+        this.changeColor(),
+        this.genQuote()
+    }
 })
-var color = randomColor();
-console.log(color);
